c48f88fefb3e67ef8a1ae47f8427eb5600f1105d
Arnaldo Assis Ferreira <arnaldo.ferreira.e@riachuelo.com.br>
2022-07-29 22:28:57 -0300

    container

--- src/routes.js
+++ src/routes.js
@@ -5,20 +5,33 @@ import Dashboard from "./pages/dashboard/dashboard";
 import ListaFazenda from "./pages/listaFazenda/listaFazenda";
 import FormFazenda from "./pages/formFazenda/formFazenda";
 import Navbar from './components/Navbar/Navbar';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+
+import Container from '@mui/material/Container';
 
 const Rotas = () => {
-   
-   return(
-    <BrowserRouter>
-        <Navbar />
-        <Routes>
-           <Route element={<Dashboard />}  path="/" exact />
-           <Route element={<Dashboard />}  path="/dashboard"  />
-           <Route element={<ListaFazenda />} path="/listaFazenda" />
-           <Route element={<FormFazenda />} path="/formFazenda" />
-        </Routes>
-    </BrowserRouter>
-       
+
+   return (
+      <BrowserRouter>
+         <Navbar />
+         <Container maxWidth="xl" sx={{ marginTop: 2 }}>
+            <Card sx={{ minWidth: 275 }}>
+               <CardContent>
+                  <Routes>
+                     <Route element={<Dashboard />} path="/" exact />
+                     <Route element={<Dashboard />} path="/dashboard" />
+                     <Route element={<ListaFazenda />} path="/listaFazenda" />
+                     <Route element={<FormFazenda />} path="/formFazenda" />
+                  </Routes>
+               </CardContent>
+            </Card>
+         </Container>
+      </BrowserRouter>
+
    )
 }
 
